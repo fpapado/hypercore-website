2b2be3250df11f551999d452baf40ac8f3b71209
Fotis Papadogeorgopoulos <fotis@futurice.com>
2021-03-01 11:05:49 +0200

    Make mobile menu keyboard-operable, work with resize

--- js/mobile-nav.js
+++ js/mobile-nav.js
@@ -1,5 +1,9 @@
 var menu = document.getElementById('mobile-menu')
 document.getElementById('mobile-menu-btn').addEventListener('click', function (e) {
   e.preventDefault()
+
+  var menuButton = e.currentTarget
+  var previousState = menuButton.getAttribute('aria-expanded')
+  menuButton.setAttribute('aria-expanded', previousState === 'true' ? 'false' : 'true')
   menu.classList.toggle('open')
 })
